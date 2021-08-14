b36857fa0e76346f6449a050a7d3be78dd52abb3
daizou703 <daizou.oz@gmail.com>
2021-08-14 22:33:07 +0900

    2nd commit

--- main.py
+++ main.py
@@ -1,19 +1,29 @@
 import streamlit as st
-import numpy as np
-import pandas as pd
-from PIL import Image
+import time
 
 st.title('Streamlit 超入門')
 
-st.write('DataFrame')
+st.write('プログレスバーの表示')
+'Start!!!'
 
-st.write('Display Image')
+latest_iteration = st.empty()
+bar = st.progress(0)
 
-img = Image.pen('sample.jpg')
-st.image(img, caption='sample', use_column_width=true)
+for i in range(100):
+    latest_iteration.text(f'Iteration {i+1}')
+    bar.progress(i + 1)
+    time.sleep(0.1)
 
-# df = pd.DataFrame(
-#     np.random.rand(100, 2)/[50, 50] + [35.69, 139.70],
-#     columns=['lat', 'lon']
-# )
-# st.map(df)
+'Done!!!!'
+
+left_column, right_column = st.beta_columns(2)
+button = left_column.button('右カラムに文字を表示')
+if button:
+    right_column.write('ここは右カラム')
+
+expander1 = st.beta_expander('問い合わせ1')
+expander1.write('問い合わせ1の回答')
+expander1 = st.beta_expander('問い合わせ2')
+expander1.write('問い合わせ2の回答')
+expander1 = st.beta_expander('問い合わせ3')
+expander1.write('問い合わせ3の回答')
